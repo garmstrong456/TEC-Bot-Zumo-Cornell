d480e5ac3bd09753a4066fcfde8ee664d6c791f2
Greg Armstrong <garmstrong456@gmail.com>
2016-07-05 03:04:35 -0400

    added motor_test.ino

---
+++ daily code/Day 2/motor_test/motor_test.ino
@@ -0,0 +1,39 @@
+/*
+ * Motor Test
+ * Greg Armstrong, July 5th 2016
+ * 
+ * License: CC BY-SA 4.0
+ * 
+ * Drive the TEC-Bot back and forth at full speed
+ * This can be used to test the motor direction on your robot
+ * The user LED should be on while the robot moves forward and
+ * off while it moves backward
+ */
+
+#define LED_PIN 13
+
+ZumoMotors motors;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+
+  //If your robot turns in circles or moves backwards when it should be going forward uncomment
+  //one or both of these lines
+  
+  //motors.flipLeftMotor(true);
+  //motors.flipRightMotor(true);
+
+}
+
+void loop() {
+  //Drive forward for 2 seconds
+  motors.setLeftSpeed(400);
+  motors.setRightSpeed(400);
+  digitalWrite(LED_PIN, HIGH);      //Turn the LED on when the robot moves forward
+  delay(2000);
+
+  motors.setLeftSpeed(-400);
+  motors.setRightSpeed(-400);
+  digitalWrite(LED_PIN, LOW);      //Turn the LED off when the robot moves backward
+  delay(2000);
+}
